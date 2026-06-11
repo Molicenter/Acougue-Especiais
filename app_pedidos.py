b081c40c86e2533486399960102e8ae4892178cf
adrianormartins86-lab <adrianormartins86@gmail.com>
2026-06-11 16:29:28 -0300

    Update app_pedidos.py

--- app_pedidos.py
+++ app_pedidos.py
@@ -9,7 +9,7 @@ from streamlit_gsheets import GSheetsConnection
 # ─────────────────────────────────────────────
 st.set_page_config(
     page_title="Gestão de Pedidos - Açougue Especial",
-    page_icon="🍖",
+    page_icon="🍗",
     layout="wide",
     initial_sidebar_state="expanded"
 )
